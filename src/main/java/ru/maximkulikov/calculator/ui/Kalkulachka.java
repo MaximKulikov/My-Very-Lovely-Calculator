13b10674fae0b01f39013638621421bbee6dd7ea
MaximKulikov <info@maximkulikov.ru>
2018-09-01 17:12:18 +0300

    show basic html

--- src/main/java/ru/maximkulikov/calculator/ui/Kalkulachka.java
+++ src/main/java/ru/maximkulikov/calculator/ui/Kalkulachka.java
@@ -1,8 +1,12 @@
 package ru.maximkulikov.calculator.ui;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javafx.fxml.FXML;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import ru.maximkulikov.calculator.Calculator;
 
 /**
  * Lovely-Calculator
@@ -12,13 +16,19 @@ import javafx.scene.web.WebView;
  */
 public class Kalkulachka {
 
+    private static final String INDEX_HTML = "web/index.html";
+
     @FXML
     private WebView webView;
 
     private WebEngine webEngine;
 
     @FXML
-    private void initialize() {
+    private void initialize() throws Exception {
         //Not Yet realize
+        webEngine = webView.getEngine();
+        Path url = Paths.get(System.getProperty("user.dir"), "exterene/index.html");
+
+        webEngine.load(Calculator.class.getResource(INDEX_HTML).toExternalForm());
     }
 }
